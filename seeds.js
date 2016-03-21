f3ed7b9be320a2a9c7479b3cd586a71ec8f06af1
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-20 20:15:02 -0400

    Completed seed.js. Created models and stock users, comments and items

--- seeds.js
+++ seeds.js
@@ -12,6 +12,7 @@ var itemSchema = new Schema({
     ref: 'User'
   },
   itemName: String;
+  itemDescription: String;
   itemPrice: Number,
   itemSold: Boolean,
   // comments needs to be an array because an item can have several comments - connecting item to comments
@@ -35,6 +36,7 @@ var userSchema = new Schema({
   }]
 });
 
+//Add Hash and Bcrypt to user password
 userSchema.pre('save', function(next){
   var user = this;
   //only hash the password if it has been modified or is new
@@ -52,5 +54,135 @@ userSchema.pre('save', function(next){
     });
   });
 });
+
+//Create comment document
+var commentSchema = new Schema({
+  //Connecting comment to User
+  _owner: {
+    type: Schema.Types.ObjectId
+    ref: 'User'
+  },
+  itemLink: String,
+  commentMessage: String
+});
+
+// Setting all models
+Item = mongoose.model('Item', itemSchema);
+User = mongoose.model('User', userSchema);
+Comment = mongoose.model('Comment', commentSchema);
+
+// Create Seed Users, Items, Comments
+var userOne = newUser({
+  username:'johnsmith',
+  password:'password',
+  money: 10000,
+  collectedItems:[]
+});
+
+//Save comments and items to userOne into DB
+userOne.save(function(err){
+  if (err) throw err;
+
+  //Inputing Comments
+  var commentOne: new Comment({
+    _owner: userOne.id,
+    itemLink: 'Monopoly'
+    commentMessage: 'Simply a classic'
+  });
+
+   var commentTwo: new Comment({
+    _owner: userOne.id,
+    itemLink: 'Clue'
+    commentMessage: 'Loved this growing up!'
+  });
+
+  commentOne.save(function(err){
+    if(err) throw err;
+    commentTwo.save(function(err){
+       if(err) throw err;
+    });
+  });
+
+   //Inputing Items
+
+   var itemOne = new Item({
+    itemName: 'Monopoly',
+    itemDescription:'Players take the part of land owners, attempting to buy and then develop their land. Income is gained by other players visiting their properties and money is spent when they visit properties belonging to other players',    itemPrice: 5000,
+    itemSold: false
+   });
+
+   itemOne.save(function(err){
+    if(err) throw err;
+   //Connecting Comments to items
+    Item.update({itemName: 'Monopoly'}, {$push: {comments: commentOne._id}})
+      .exec(function(err){
+        if(err) throw err;
+        console.log('successful');
+      });
+    });
+
+   var itemTwo = new Item({
+    itemName: 'Clue',
+    itemDescription: 'The classic detective game! In Clue, players move from room to room in a mansion to solve the mystery of: who done it, with what, and where?',
+    itemPrice: 4000,
+    itemSold: false
+   });
+
+   itemTwo.save(function(err){
+    if(err) throw err;
+   //Connecting Comments to items
+    Item.update({itemName: 'Clue'}, {$push: {comments: commentTwo._id}})
+      .exec(function(err){
+        if(err) throw err;
+        console.log('successful');
+      });
+    });
+});//End UserOne Creation
+
+// Create Seed UserOne
+var userTwo = newUser({
+  username:'janesmith',
+  password:'password',
+  money: 15000,
+  collectedItems:[]
+});
+
+userTwo.save(function(err){
+  if (err) throw err;
+
+  //Inputing Comments
+  var commentOne: new Comment({
+    _owner: userTwo.id,
+    itemLink: 'Monopoly'
+    commentMessage: 'I always got sent to Jail!'
+  });
+
+   var commentTwo: new Comment({
+    _owner: userTwo.id,
+    itemLink: 'Clue'
+    commentMessage: 'Prof. Plum in the Conservatorty with the Wrench!'
+  });
+
+  commentOne.save(function(err){
+    if(err) throw err;
+    commentTwo.save(function(err){
+       if(err) throw err;
+    });
+  });
+
+   //Connecting Comments to items
+  Item.update({itemName: 'Monopoly'}, {$push: {comments: commentOne._id}}) //here comments refers to comments section of the itemSchema
+    .exec(function(err){
+      if(err) throw err;
+      console.log('successful');
+    })
+
+   //Connecting Comments to items
+  Item.update({itemName: 'Life'}, {$push: {comments: commentTwo._id}})
+    .exec(function(err){
+      if(err) throw err;
+      console.log('successful');
+    });
+});//End UserTwoCreation
 
 
